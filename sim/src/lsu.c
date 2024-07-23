19c22ab8c79c2b9fb04a784805836e6d05e9d823
lolo2438 <lolo2438@hotmail.com>
2024-07-23 08:49:11 -0400

    work on lsu functions

--- sim/src/lsu.c
+++ sim/src/lsu.c
@@ -2,6 +2,127 @@
 #include "mem.h"
 #include "RV32I.h"
 
+/* Tasks:
+ * -> Buffer Store and loads
+ *    Load/Store buffer:
+ *      1. Push store on the L/S Buffer write pointer
+ *      2. Set bits according to
+ *    Function:
+ *      lsu_shed_load/store()
+ *
+ *    In order tracker:
+ *    Check for stores that are done
+ *
+ * -> Shedule
+ *
+ *
+ *  If previous load at same addr new store -> Wait for load to be done
+ *  If previous store ad same addr new load -> foward to load
+ *
+ * Store Check Mask:
+ *  - When store op in Store Buffer, set SCM
+ *  -
+ */
+
+
+/* ** STORE **
+ * Instruction:
+ *      sw rs2, imm(rs1)
+ *
+ * store buffer:
+ *      Fields:
+ *      ADDR            = imm + rs1
+ *      DATA            = rs2
+ *      DATA_SRC        = address to watch for on cdb if data not ready
+ *      BUSY            = store buffer entry is used
+ *      ADDR_READY      = address field is valid
+ *      DATA_READY      = data field is valid
+ *      ISSUED          = Store has been issued to memory, busy can be cleared
+ *
+ *      Logic:
+ *      1. An entry is created for the store instruction in the store buffer | lsu_shed_store
+ *             - if [store address is available]{
+ *                 addr = store_addr, addr_ready = true
+ *               } else {
+ *                 addr_ready = false
+ *               }
+ *             - if [store data is available]{
+ *                 data = store_data, data_ready = true
+ *               } else {
+ *                 data_src = rob_addr, data_ready = false
+ *               }
+ *             - busy = true, issued = false
+ *             - The address of the store buffer entry is returned to be used by the issue buffer for the address µop
+ *             - Clear the load_buffer.STORE_DEPENDANCE_MASK bit for the entry since it's a new store
+ *
+ *      2. When addr_ready = true and data_ready = true | lsu_exec
+ *              - if [NOT( (younger store has no address) OR
+ *                         (older store has same address AND older store not issued) OR
+ *                         (L/S not executed before a fence instruction) )] {
+ *                              issue the store, ISSUED=true, BUSY=false,
+ *                              if [there is a load with STORE_SPECULATIVE flag AND store_addr == load_addr]
+ *                                      load_buffer.DATA = store_data
+ *                                      load_buffer.VALID = true
+ *                         }
+ *
+ * ** LOAD **
+ * Instruction
+ *      lw rs2, imm(rs1)
+ * Load buffer:
+ *      Fields:
+ *      ADDR = imm + rs1
+ *      DATA = mem[addr]
+ *      DATA_DEST = rob_entry
+ *      BUSY
+ *      ADDR_READY
+ *      ISSUED
+ *      VALID
+ *      STORE_DEPENDANCE_MASK
+ *      STORE_SPECULATIVE
+ *
+ *      Logic:
+ *      1. An entry is created in the load buffer for a load instruction | lsu_shed_load
+ *              - if [load addr available]{addr = load_addr, addr_ready = true} else {addr_ready = false}
+ *              - busy = true, sleep = false, issued = false
+ *              - store_mask = all stores that are busy
+ *              - DATA_DEST = rob_entry
+ *              - the address of the load buffer is returned to be used by the issue buffer for the address µop
+ *
+ *      2. When addr_ready = true | lsu_exec
+ *              - if [address of store_dependance_mask & store_buffer AND no FENCE before] {
+ *                      // Match
+ *                      if [address match] {
+ *                              DATA = oldest issued store_buffer.data
+ *                              valid = true
+ *                      } else {
+ *                              issue load, issued = true;
+ *                              valid = false
+ *                      }
+ *
+ *                      // There is at least 1 older store that is unknowned
+ *                      if [store_address missing] {
+ *                              store_speculative = true;
+ *                              valid = false;
+ *                      }
+ *                }
+ *
+ *      3. When load_buffer.data comes back to the LSU
+ *              if [issued == true] {
+ *                      data = mem[data]
+ *              }
+ *
+ *              the load will now wait until store_speculative flag goes down before valid = 1
+ *
+ *      4. when VALID = true
+ *              - busy = false
+ *              - send data back to execution engine
+ *
+ *
+ * ** FENCE **
+ *
+ */
+
+
 static struct lsu_t{
 
         int lb_size;
@@ -185,3 +306,5 @@ int lsu_exec(void) {
 int lsu_wb(struct cdb_data data) {
 
 }
+
+
